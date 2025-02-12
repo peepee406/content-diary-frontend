88dba10ab4367c22257cd57b6e62438bc4e2fb54
peepee406 <71884583+peepee406@users.noreply.github.com>
2025-02-13 05:21:03 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,4 @@
-export default function Home() {
-  return (
-    <div className="bg-gray-900 text-white min-h-screen p-8">
-      <h1 className="text-4xl font-bold mb-6">My Content Diary</h1>
-    </div>
-  );
-}
+
 import { useState } from "react";
 import axios from "axios";
 
