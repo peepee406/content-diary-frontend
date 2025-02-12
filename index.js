fcbd9fe4294b39830c023a3b7a0f9aaa1626b1b2
peepee406 <71884583+peepee406@users.noreply.github.com>
2025-02-13 04:21:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-gray-900 text-white min-h-screen p-8">
+      <h1 className="text-4xl font-bold mb-6">My Content Diary</h1>
+    </div>
+  );
+}
